1a62935f8cbce4f878da304c7803bc09fda755e5
cdvillamil92 <cvillamil@dayscript.com>
2019-04-02 16:37:22 -0500

    mail

--- resources/views/correo.blade.php
+++ resources/views/correo.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 <div class="container-fluid">
     <div class="row">
-    	<p>Nuestro cliente {{ $data['nombres'] }} identificado con {{ $data['tipo_documento'] }} {{ $data['cedula'] }} y cuyo teléfono de contacto es {{ $data['celular'] }} confirmó interés en adquirir los siguientes seguros adicionales:</p>
-        <p><strong>{{ $data['salud'] }}</strong></p>
-        <p><strong>{{ $data['vida'] }}</strong></p>
-        <p><strong>{{ $data['hogar'] }}</strong></p>
-        <p><strong>{{ $data['autos'] }}</strong></p>
+    	<p>Nuestro cliente <strong>{{ $data['nombres'] }}</strong> identificado con <strong>{{ $data['tipo_documento'] }}</strong> número <strong>{{ $data['cedula'] }}</strong> cuyo teléfono de contacto es <strong>{{ $data['celular'] }}</strong> y con correo electrónico <strong>{{ $data['correo'] }}</strong> confirmó interés en adquirir los siguientes seguros adicionales:</p>
+    	@if (isset($data['salud']))
+        <p><strong>Salud</strong></p>
+        @endif
+    	@if (isset($data['vida']))
+        <p><strong>Vida</strong></p>
+        @endif
+    	@if (isset($data['hogar']))
+        <p><strong>Hogar</strong></p>
+        @endif
+    	@if (isset($data['autos']))
+        <p><strong>Autos</strong></p>
+        @endif
     </div>
 </div>
