b5a72d2c4894676c511a8314243cebfb4c4c7296
cdvillamil92 <cvillamil@dayscript.com>
2019-04-11 14:13:25 -0500

    actualizacion base de datos

--- resources/views/correo.blade.php
+++ resources/views/correo.blade.php
@@ -3,16 +3,16 @@
     <div class="row">
     	<p>Nuestro cliente <strong>{{ $data['nombres'] }}</strong> identificado con <strong>{{ $data['tipo_documento'] }}</strong> número <strong>{{ $data['cedula'] }}</strong> cuyo teléfono de contacto es <strong>{{ $data['celular'] }}</strong> y con correo electrónico <strong>{{ $data['correo'] }}</strong> confirmó interés en adquirir los siguientes seguros adicionales:</p>
     	@if (isset($data['salud']))
-        <p><strong>Salud</strong></p>
+        <p><strong>Salud, </strong></p>
         @endif
     	@if (isset($data['vida']))
-        <p><strong>Vida</strong></p>
+        <p><strong>Vida, </strong></p>
         @endif
     	@if (isset($data['hogar']))
-        <p><strong>Hogar</strong></p>
+        <p><strong>Hogar, </strong></p>
         @endif
     	@if (isset($data['autos']))
-        <p><strong>Autos</strong></p>
+        <p><strong>Autos, </strong></p>
         @endif
     </div>
 </div>
